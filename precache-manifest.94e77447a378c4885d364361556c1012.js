97f48aa480471f36c9b8295793b9631fb1d672c1
DmitryKRTV <dmitrykorotaev.job@gmail.com>
2022-12-19 15:40:43 +0300

    Updates

---
+++ precache-manifest.94e77447a378c4885d364361556c1012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29093cf46c0af8b7c27e3fcef3c4cf0",
+    "url": "/it-incubator-todolist-ts-01/index.html"
+  },
+  {
+    "revision": "f4063882d08cf01960f3",
+    "url": "/it-incubator-todolist-ts-01/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "c54787b64f522eebf76b",
+    "url": "/it-incubator-todolist-ts-01/static/js/2.985f8dbb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/it-incubator-todolist-ts-01/static/js/2.985f8dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4063882d08cf01960f3",
+    "url": "/it-incubator-todolist-ts-01/static/js/main.9a781fa9.chunk.js"
+  },
+  {
+    "revision": "ddb6461b948278a5bf95",
+    "url": "/it-incubator-todolist-ts-01/static/js/runtime-main.565a4bd4.js"
+  }
+]);
